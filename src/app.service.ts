368674f8f660539a17bb2215b30b53bb2206306e
nikhil27apr <71115716+nikhil27apr@users.noreply.github.com>
2026-03-30 17:37:59 +0000

    added CI pipeline

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! this is nv ji what is going on bro';
   }
 }
